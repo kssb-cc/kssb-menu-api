4ff47c19b08652ba7417a8ef0eab56c002909483
braillescreen <patrick@pwry.net>
2022-09-14 13:17:49 -0500

    Support for getting an indevidual day (finally).

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import flask, kssbmenu
 from flask import render_template, request
 
+m = kssbmenu.kssb_menu()
 app = flask.Flask(__name__)
 
 @app.route("/")
@@ -9,8 +10,14 @@ def home():
 
 @app.route("/api/get_raw")
 def get_menu():
-	m = kssbmenu.kssb_menu()
 	return m.download()
 
+@app.route("/api/get")
+def get():
+	day = request.args.get("day")
+	print(f"Request for day: {day}")
+	tempmenu = m.download()
+	if day == "" or day not in tempmenu: return "Wrong day"
+	return str(tempmenu[day])
 
 app.run()
